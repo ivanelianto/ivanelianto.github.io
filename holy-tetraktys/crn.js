7ed51c6f174310707266aea39872f08f0400e26f
Ivan <purenaturalism@gmail.com>
2024-02-17 23:36:00 +0700

    feat: add combined root

---
+++ holy-tetraktys/crn.js
@@ -0,0 +1,32 @@
+const crnList = [
+  `The vibrations of Independence will take effect on
+both of you. Both of you are very independent with one another. Both of you do
+not depend or rely on each other financially and/or socially. Both have separate
+activities and both of you enjoys freedom.`,
+  `Both of you will freely express each other’s opinion Both will
+not hesitate to express what you don't like of each other. Emotions rule both of
+you. An expressive yet emotional kind of relationship. As such the tendency of not
+tactful with each other and towards others too.`,
+  `Aggressive, Impatient rules. As such, both of you will do things in a hurry and
+always wanting to get things fast. Definitely, Temper flares more often than not. At
+times can be sensitive. Spiritual interest between both of you may arise.`,
+  `Intelligent ideas will come out of this combination. Both will easily create reading
+interest and other interest of sorts. An organized team but a straightforward
+relationship. Both can mingle very easily in a crowd.`,
+  `When both of you are together, obstacles and
+difficulties are bound to happen. Both of you will always be 'moving here and there’. More often than not, both are always seeking for best. Unpredictable hurdles await both of you and as such, both will worry unnecessarily.`,
+  `Both do not depend on each other financially and both will plan carefully as far as
+finance is concerned. Both of you will always be at comfort zone. But be careful! On
+the other negative side, it may be that both could have financial obstacles. A family
+oriented relationship.`,
+  `As such, many friends will surround both of you. More often,
+these friends are your supporters. Because of the many friends, both will enjoy an
+active life‐style. An entertaining relationship. Luck will always be on both of you.
+The negative side could be, both may take advantage of friends. When comes to
+argument with others, both will be the winning edge.`,
+  `A stressful and ‘not focused’ relationship. Always doing something, which are
+unnecessary. Both will always find ways and means to do something but often end
+up in vain. Both will have many opportunities to travel.`,
+  `A business oriented relationship but the negative side could be a greedy team. As such, Power & Creativity will prevail and
+whenever both of you are together, both will be recognized.`,
+];
