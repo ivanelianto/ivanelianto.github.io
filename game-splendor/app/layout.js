fa71ebdcedcde9f1923742691d98eeefdca87023
Ivan Elianto <purenaturalism@gmail.com>
2026-08-17 00:13:58 +0700

    chore(font): fix cross origin attribute

--- game-splendor/app/layout.js
+++ game-splendor/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="id">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Mystery+Quest&family=Playpen+Sans&family=Kablammo&display=swap" rel="stylesheet" />
       </head>
       <body>
